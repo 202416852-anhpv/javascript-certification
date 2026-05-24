9259051089cd2222248b488b68983c043906c2ad
Pham Viet Anh <pvanh780@gmail.com>
2026-05-24 17:12:29 +0700

    feat: complete markdown to html converter certification

--- markdown-to-html-converter/script.js
+++ markdown-to-html-converter/script.js
@@ -1,3 +1,27 @@
 const markDownInput = document.getElementById("markdown-input");
 const htmlOuput = document.getElementById("html-output");
 const preview = document.getElementById("preview");
+
+function convertMarkdown() {
+  let text = markDownInput.value;
+
+  text = text.replace(/^#\s+(.*)$/gm, "<h1>$1</h1>");
+  text = text.replace(/^##\s+(.*)$/gm, "<h2>$1</h2>");
+  text = text.replace(/^###\s+(.*)$/gm, "<h3>$1</h3>");
+  text = text.replace(/\*\*(.*)\*\*/g, "<strong>$1</strong>");
+  text = text.replace(/__(.*)__/g, "<strong>$1</strong>");
+  text = text.replace(/\*(.*)\*/g, "<em>$1</em>");
+  text = text.replace(/_(.*)_/g, "<em>$1</em>");
+  text = text.replace(/!\[(.*)\]\((.*)\)/g, '<img alt="$1" src="$2">');
+  text = text.replace(/\[(.*)\]\((.*)\)/g, '<a href="$2">$1</a>');
+  text = text.replace(/^>\s+(.*)$/gm, "<blockquote>$1</blockquote>");
+
+  return text;
+}
+
+markDownInput.addEventListener("input", () => {
+  const htmlResult = convertMarkdown();
+
+  htmlOuput.textContent = htmlResult;
+  preview.innerHTML = htmlResult;
+});
