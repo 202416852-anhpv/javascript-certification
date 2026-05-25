a60cd0d1d4d803811ef7aad748921fcbae38b2ce
Pham Viet Anh <pvanh780@gmail.com>
2026-05-25 09:41:43 +0700

    feat: complete drum machine certification

--- drum-machine/script.js
+++ drum-machine/script.js
@@ -1,1 +1,32 @@
 const drumPads = document.querySelectorAll(".drum-pad");
+const display = document.getElementById("display");
+
+function playAudio(audioElement, parentId) {
+  if (!audioElement) return;
+
+  audioElement.currentTime = 0;
+  audioElement.play();
+
+  const displayName = parentId.replace(/-/g, " ");
+
+  display.textContent = displayName;
+}
+
+drumPads.forEach((pad) => {
+  pad.addEventListener("click", () => {
+    const audio = pad.querySelector(".clip");
+
+    playAudio(audio, pad.id);
+  });
+});
+
+document.addEventListener("keydown", (event) => {
+  const keyStr = event.key.toUpperCase();
+  const audio = document.getElementById(keyStr);
+
+  if (audio) {
+    const parentId = audio.parentElement;
+
+    playAudio(audio, parentId.id);
+  }
+});
